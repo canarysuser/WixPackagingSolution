5333e4692d7e1d2ccd68b0a531cd8dee024643ea
Sudhir <sudhir.nayak.b@gmail.com>
2026-05-13 11:09:13 +0530

    Add project files.

---
+++ SampleMVCWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleMVCWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
