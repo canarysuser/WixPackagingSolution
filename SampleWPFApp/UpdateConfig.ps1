448ddace47723699c01e80a7ad1e9d976ecf04a2
Sudhir <sudhir.nayak.b@gmail.com>
2026-05-14 16:43:03 +0530

    Final Commit

---
+++ SampleWPFApp/UpdateConfig.ps1
@@ -0,0 +1,16 @@
+$sessionData=$env:CustomActionData
+
+$values=@{} 
+
+$sessionData.Split(";") | ForEach-Object {
+	$pair = $_.Split("=")
+	$values[$pair[0]]=$pair[1]
+}
+$port =$values["PORT"]
+$installDir=$values["INSTALLDIR"]
+
+$configFile = Join-Path $installDir "appsettings.json"
+$json = Get-Content $configFile -Raw | ConvertFrom-Json
+$json.GrpcHttp2.Url="http://*:" + $port
+$json | Convert-Json -Depth 10 | Set-Content $configFile 
+
